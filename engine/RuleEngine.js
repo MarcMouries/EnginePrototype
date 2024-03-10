5468cdb3300a27092e97dadb623f691ccb31c31a
MarcMouries <marc@mouries.net>
2024-03-10 16:21:55 -0400

    + createFactProxy

--- engine/RuleEngine.js
+++ engine/RuleEngine.js
@@ -16,15 +16,48 @@ export class RuleEngine {
   constructor() {
     this.facts = {};
     this.rules = [];
+    this.factChanges = {}; // Track the last update times
   }
 
   addFact(object) {
-    this.facts[object.id] = object;
+    const proxy = this.createFactProxy(object);
+    this.facts[object.id] = proxy;
     this.evaluateRules();
   }
   addRule(rule) {
     this.rules.push(rule);
   }
+
+  createFactProxy(object) {
+    const ruleEngine = this;
+    const handler = {
+      set: (obj, prop, value) => {
+        const changeKey = `${obj.id}_${prop}`;
+        //console.log("Proxy: changeKey: " + changeKey);
+
+        // Only trigger evaluation if the new value is different from the current value
+        if (obj[prop] !== value) {
+         // console.log(`RuleEngine: Fact ${obj.id} property ${prop} changed\n\tprevious value = ${obj[prop]}\n\tnew value = ${value}`);
+         // console.log(`RuleEngine: factChanges = `, this.factChanges);
+         // console.log(`RuleEngine: facts = `, this.facts);
+          
+          // Only proceed if this is a new or updated change
+          if (!ruleEngine.factChanges[changeKey]) {
+            obj[prop] = value;
+            obj._timechanged = Date.now();
+
+            // Mark the fact and property as changed with the current timestamp
+            ruleEngine.factChanges[`${obj.id}_${prop}`] = Date.now();
+            console.log(`RuleEngine: factChanges = `, this.factChanges);
+        //    ruleEngine.evaluateRules();
+          }
+        }
+        return true; // Always return true
+      },
+    };
+    return new Proxy(object, handler);
+  }
+
   updateFact(object) {
     if (this.facts[object.id]) {
       this.facts[object.id] = object;
@@ -36,12 +69,18 @@ export class RuleEngine {
 
   evaluateRules() {
     this.rules.forEach((rule) => {
+
       Object.values(this.facts).forEach((fact) => {
+
         const isConditionMet = rule.condition(fact);
+      
         if (isConditionMet) {
+          //console.error(`Rule Engine.evaluateRules() - rule ${rule.name} evaluated to true.\n\t Fact =  ${fact.constructor.name}\n` , rule, fact);
           rule.action(fact);
         }
       });
     });
+    // Reset change flags after evaluation
+    this.factChanges = {};
   }
 }
--- test/Test_RuleEngine.js
+++ test/Test_RuleEngine.js
@@ -17,37 +17,64 @@ ruleEngine.addFact(sensor);
 
 // Initialize Message object with initial text
 class Message {
-  constructor(status = "") {
+  constructor(id, status = "") {
+    this.id = id;
     this.status = status;
   }
 }
-let message = new Message();
+let message = new Message("message");
 ruleEngine.addFact(message);
 
 const ruleSet = [
   {
     name: "HighTemperatureAlert",
     condition: (fact) => fact.type === "TemperatureSensor" && fact.temperature > 30,
-    action: (fact) => console.log(`It's really hot. Current temperature is ${fact.temperature}°C on sensor ${fact.id}`),
+    action: (fact) => {
+      const message = ruleEngine.facts['message']; // getting 'message' with the ID/key for the Message object
+      message.status = `It's really hot. Current temperature is ${fact.temperature}°C on sensor ${fact.id}`;
+    }
   },
   {
     name: "FreezingTemperatureAlert",
     condition: (fact) => fact.type === "TemperatureSensor" && fact.temperature <= 0,
-    action: (fact) => console.log(`It's freezing. Current temperature is ${fact.temperature}°C on sensor ${fact.id}`),
+    action: (fact) => {
+      const message = ruleEngine.facts['message'];
+      message.status = `It's freezing. Current temperature is ${fact.temperature}°C on sensor ${fact.id}`;
+    },
   },
   {
+    name: "NormalTemperatureAlert",
+    condition: (fact) => fact.type === "TemperatureSensor" && fact.temperature >= 10 && fact.temperature <= 25,
+    action: (fact) => {
+      const message = ruleEngine.facts['message'];
+      message.status = "Temperature has normalized. 10 ≤ T ≤ 25";
+    }
+  }
+,
+  {
     name: "MessageChangeAlert",
-    condition: (fact) => fact.type === "Message" && fact.status !== "",
-    action: (fact) => console.log("MessageChangeAlert: " + fact.status),
+    condition: (fact) => fact instanceof Message && fact.status !== "",
+    action: (fact) => console.log("📖 => Rule: MessageChangeAlert: " + fact.status),
+  },
+  {
+    name: "TemperatureFluctuationAlert",
+    condition: (fact) => fact.id === "message" && (fact.status.includes("hot") || fact.status.includes("freezing")),
+    action: (fact) => {
+      // This rule aims to check if there was a rapid change in temperature conditions.
+      // However, given the current design of the RuleEngine, it might not effectively handle or recognize
+      // the sequence of temperature changes if they happen within a single evaluation cycle.
+      console.log(`🚨 => Rule: TemperatureFluctuationAlert: Detected rapid temperature change in messages: "${fact.status}"`);
+    }
   }
 ];
 
 ruleSet.forEach(rule => {  ruleEngine.addRule(rule);});
 
-
-// Update the sensor's temperature to 31°C
 sensor.temperature = 31;
 ruleEngine.updateFact(sensor);
 
 sensor.temperature = 0;
+ruleEngine.updateFact(sensor);
+
+sensor.temperature = 20;
 ruleEngine.updateFact(sensor);
